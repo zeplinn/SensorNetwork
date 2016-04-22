a079e9568feefbef3769cc2db72763df928dd9ef
jesper jensen <jrytterj@hotmail.com>
2016-04-22 16:03:58 +0200

    qwq

---
+++ Scripts/Bridge/output/scripts.js
@@ -0,0 +1,19 @@
+﻿(function (globals) {
+    "use strict";
+
+    Bridge.define('Scripts.App', {
+        statics: {
+            config: {
+                init: function () {
+                    Bridge.ready(this.main);
+                }
+            },
+            main: function () {
+                // Simple alert() to confirm it's working
+                Bridge.global.alert("Success");
+            }
+        }
+    });
+    
+    Bridge.init();
+})(this);
---
+++ src/SensorNetwork/wwwroot/js/jrj-tabs.js
@@ -0,0 +1,12 @@
+﻿//jquery tabs Plugin
+
+(function ($) {
+    
+    function JrjTabs(placeholder) {
+        $(placeholder).append('<div class="jrj-tabs-bar"></div>')
+
+        var tabs = this,
+
+    }
+    
+})(jQuery);
